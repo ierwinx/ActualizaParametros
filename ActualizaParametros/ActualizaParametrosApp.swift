0da253534673f7b235f346f9892ee73a52b63b5d
Eяw!и Gontier <ierwinx@me.com>
2022-07-04 10:21:50 -0500

    Initial Commit

---
+++ ActualizaParametros/ActualizaParametrosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActualizaParametrosApp.swift
+//  ActualizaParametros
+//
+//  Created by Erwin Luz León on 04/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ActualizaParametrosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
